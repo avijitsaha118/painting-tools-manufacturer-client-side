06792cc6e3be469c0fe618a4918c103bd5d3df2a
avijitsaha118 <sahaavijit908@gmail.com>
2022-05-26 23:25:55 +0600

    banner updated

--- src/pages/Home/Banner.js
+++ src/pages/Home/Banner.js
@@ -8,7 +8,7 @@ const Banner = () => {
       <div class="hero-content text-center text-neutral-content">
         <div class="max-w-md">
           <h1 class="mb-5 text-5xl font-bold">Hello there</h1>
-          <p class="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+          <p class="mb-5">Welcome to our website. You can enjoy your shopping from here. Now we opperate more than seven country. Stay calm and Stay with us. Plase explore.</p>
           <button class="btn btn-primary">Get Started</button>
         </div>
       </div>
